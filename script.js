55bf5ab9e9546e1d6fe68cb69c5305cbc2290038
jha19mayank <60752983+jha19mayank@users.noreply.github.com>
2021-09-08 08:59:04 +0530

    📝added functionality to this project

---
+++ script.js
@@ -0,0 +1,53 @@
+var initialPrice = document.querySelector("#initial-price");
+var currentPrice = document.querySelector("#current-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var calculateBtn = document.querySelector("#calculate-btn");
+var outputBox = document.querySelector("#output-box");
+
+calculateBtn.addEventListener("click", function calculationHandler() {
+  var initial = Number(initialPrice.value);
+  var quantity = Number(stocksQuantity.value);
+  var current = Number(currentPrice.value);
+
+  if ((initial === 0) | (quantity === 0) | (current === 0)) {
+    showOutput("Enter all the inputs");
+  } else {
+    calculateProfitorLoss(initial, quantity, current);
+  }
+});
+
+function calculateProfitorLoss(initial, quantity, current) {
+  if (current === initial) {
+    // No profit or loss
+    showOutput("No pain No gain, No gain No pain");
+  } else if (current > initial) {
+    // profit
+    profitPerStock = current - initial;
+    profit = profitPerStock * quantity;
+    profitPercentage = (profitPerStock / initial) * 100;
+    showOutput(
+      `The profit is of ${profit} and the percentage is ${profitPercentage}%`,
+      "PROFIT"
+    );
+  } else {
+    // loss
+    lossPerStock = initial - current;
+    loss = lossPerStock * quantity;
+    lossPercentage = (lossPerStock / initial) * 100;
+    showOutput(
+      `The loss is of ${loss} and the percentage is ${lossPercentage}%`,
+      "LOSS"
+    );
+  }
+}
+
+function showOutput(message, status) {
+  outputBox.innerHTML = message;
+  if (status === "PROFIT") {
+    outputBox.style.color = "green";
+  } else if (status === "LOSS") {
+    outputBox.style.color = "red";
+  } else {
+    outputBox.style.color = "yellow";
+  }
+}
